e879d45614f5cdce81d533586fd70cf8606654ac
Tubalevião <alvaro.prata.filho@gmail.com>
2020-09-20 15:29:16 -0400

    base project files

---
+++ student/src/test/java/am/i/student/StudentApplicationTests.java
@@ -0,0 +1,13 @@
+package am.i.student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
